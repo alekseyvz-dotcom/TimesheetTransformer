d511821b2b86e8c4d0546cd929a9d66f5dde01e0
alekseyvz-dotcom <aleksey_vz@mail.ru>
2025-10-27 11:23:45 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -519,9 +519,9 @@ class ObjectTimesheet(tk.Toplevel):
         self._update_rows_days_enabled()
         self._load_existing_rows()
 
-    def _on_address_select(self, *_):
-        addr = self.cmb_address.get().strip()
-        ids = sorted(self.addr_to_ids.get(addr, []))
+def _on_address_select(self, *_):
+    addr = self.cmb_address.get().strip()
+    ids = sorted(self.addr_to_ids.get(addr, []))
 
     # Обновляем список ID и не сбрасываем выбранный, если он ещё валиден
     cur = self.cmb_object_id.get().strip()
